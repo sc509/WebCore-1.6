2db0e8d342ae84bb30a78413b4cb7ae2dbb8e440
Sv812asx <122466233+Sv812asx@users.noreply.github.com>
2023-05-14 17:07:31 +0300

    Решил проблему,когда одна из модалок не работала

--- src/js/blocks/callModal.js
+++ src/js/blocks/callModal.js
@@ -1,21 +1,19 @@
 export function setupCallModal() {
-  const call = document.querySelector('.modal-call');
-  const callButton = document.querySelector('.sidebar-menu__call-button');
-  const closeCallButton = document.querySelector('.modal-call__close-btn');
-  const modalBlock = document.querySelector('.modal__block');
+  const call = document.querySelector('.modal-call')
+  const callButton = document.querySelector('.sidebar-menu__call-button')
+  const closeCallButton = document.querySelector('.modal-call__close-btn')
 
   callButton.addEventListener('click', () => {
-    call.classList.add('modal-call--active');
-  });
+    call.classList.add('modal-call--active')
+  })
 
   closeCallButton.addEventListener('click', () => {
-    call.classList.remove('modal-call--active');
-  });
+    call.classList.remove('modal-call--active')
+  })
 
   call.addEventListener('click', (event) => {
-    if (event.target === modalBlock || modalBlock.contains(event.target)) {
-      return;
+    if (event.target.classList.contains('modal-call')) {
+      call.classList.remove('modal-call--active')
     }
-    call.classList.remove('modal-call--active');
-  });
+  })
 }
